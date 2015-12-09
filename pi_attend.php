58e380db335e42d33a4c8080c3de9093ecddbcbf
ta98330 <hare.night@gmail.com>
2015-12-09 18:22:51 +0900

    ポスト送信成功　ファイルは移動　プロキシ・ポートが書いてあるため

--- pi_attend.php
+++ pi_attend.php
@@ -2,16 +2,7 @@
 
 require "spheader.php";//ヘッダー読み込み
 
-print((empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-
-echo $_SERVER['SERVER_NAME'];
-echo 
-
-var_dump($_POST);
-/*
-$_POST['come'] = 'true';
-$_POST['user'] = '71';
-*/
+//var_dump($_POST);
 
 $pdo = new PDO("mysql:dbname={$_SESSION['dbname']}", "{$_SESSION['dbusername']}", "{$_SESSION['dbpass']}");
 $today = date("Y-m-d");
@@ -19,46 +10,22 @@ $today = date("Y-m-d");
 if(!empty($_POST['come']) or (!empty($_POST['out']))){
 
     $nowtime = date("H:i:s");
-    if(!empty($_POST['come'])){
+    
+    if(!empty($_POST['come'])){//登校時
         $st = $pdo->prepare("INSERT INTO user_logs (id, date, start_time) VALUES(?, ?, ?)");
         $st->execute(array($_POST['user'], $today, $nowtime));
         
-        $st = $pdo->query("UPDATE member SET situation = 'zaishitsu' WHERE id = {$_POST['user']}");//現状の更新
+        $st1 = $pdo->query("UPDATE member SET situation = 'zaishitsu' WHERE id = {$_POST['user']}");//現状の更新
         
-        //echo "登校時間を登録しました．",'<br /><a href="top.php">戻る</a>';
-         //header('Location: top.php');
     }
-    else{
+    else{//下校時
         $st = $pdo->prepare("UPDATE user_logs SET end_time = ? WHERE id = ? AND date = ?");
         $st->execute(array($nowtime, $_POST['user'], $today));
         
         $st = $pdo->query("UPDATE member SET situation = 'kitaku' WHERE id = {$_POST['user']}");//現状の更新
         
-        //echo "下校時間を登録しました．",'<br /><a href="top.php">戻る</a>';
-         //header('Location: top.php');
-        
-        $st = $pdo->query("UPDATE user_logs SET stay_time = TIMEDIFF(end_time,start_time) WHERE id = {$_POST['user']} AND date = '$today'");//滞在時間の計算
+        $st1 = $pdo->query("UPDATE user_logs SET stay_time = TIMEDIFF(end_time,start_time) WHERE id = {$_POST['user']} AND date = '$today'");//滞在時間の計算
         
     }
     
 }
-
-//echo $_POST['user'];
-//echo $_POST['come'];
-
-/*
-if(!empty($_POST['outreset'])){//下校取り消し
-    $st = $pdo->query("UPDATE user_logs SET end_time = NULL WHERE id = {$_POST['user']} AND date = '$today'");
-    $st = $pdo->query("UPDATE member SET situation = 'zaishitsu' WHERE id = {$_POST['user']}");//現状の更新
-    
-    //echo "下校時間を取り消しました．",'<br /><a href="top.php">戻る</a>';
-    header('Location: top.php');
-}
-*/
-
-//postリクエストのログ
-$postlog = '[POST] | '.date( "Y/m/d (D) H:i:s", time() );
-foreach ($_POST as $val) {
-$postlog .= '   |   '.$val;
-}
-dbg_log($postlog);
