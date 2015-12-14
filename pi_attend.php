e62d5dc40e2115bf446784de968804b99a5728d3
ta98330 <hare.night@gmail.com>
2015-12-14 17:33:12 +0900

    つぶやき機能

--- pi_attend.php
+++ pi_attend.php
@@ -2,9 +2,22 @@
 
 require "spheader.php";//ヘッダー読み込み
 
+// OAuthライブラリの読み込み
+require "twitteroauth/autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+//認証情報４つ
+$consumerKey = "n1lm8fBMMbW5BHuxGF8MbKv5i";
+$consumerSecret = "zQtnkslApCg2PsTc9uEvvNI7KsRa79LIJtZZpRuIdcQfc7ngPq";
+$accessToken = "3221121236-DSpEQvuGhXtkv8JnCQ3COJAZLkxLcWBqmxytTU4";
+$accessTokenSecret = "J9VvLloX8yW7Re32uwPFeAzyr5Ip4OR2aZ40uSAMrkxEy";
+
+//接続
+$connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+
 //var_dump($_POST);
 /*
-$_POST['user'] = 1210960085;
+$_POST['user'] = 1210960058;
 $_POST['pass'] = "pass";
 */
 
@@ -19,10 +32,15 @@ if($_POST['pass'] == "pass"){
             $end_time = htmlspecialchars($row['end_time']);
             $stay_time = htmlspecialchars($row['stay_time']);
         }
+    
+    $st0 = $pdo->query("SELECT * FROM member WHERE id = {$_POST['user']}");
+        while ($row = $st0->fetch()) {
+            $name = htmlspecialchars($row['name']);
+        }
 
     //echo "str",$start_time;
     //echo "end",$end_time;
-
+    
 
     $nowtime = date("H:i:s");
 
@@ -33,6 +51,16 @@ if($_POST['pass'] == "pass"){
         //echo "str",$start_time;
 
         $st2 = $pdo->query("UPDATE member SET situation = 'zaishitsu' WHERE id = {$_POST['user']}");//現状の更新
+        
+        //ツイート
+        $res = $connection->post("statuses/update", array("status" => $name.'さんが登校しました'));
+        
+        
+        
+        
+        
+        
+        
 
     }
     else if(empty($end_time)){//下校時
@@ -44,13 +72,29 @@ if($_POST['pass'] == "pass"){
         $st2 = $pdo->query("UPDATE member SET situation = 'kitaku' WHERE id = {$_POST['user']}");//現状の更新
 
         $st3 = $pdo->query("UPDATE user_logs SET stay_time = TIMEDIFF(end_time,start_time) WHERE id = {$_POST['user']} AND date = '$today'");//滞在時間の計算
+        
+        
+        //ツイート
+        $res = $connection->post("statuses/update", array("status" => $name.'さんが下校しました'));
 
     }
     else{//下校取り消し
         $st1 = $pdo->query("UPDATE user_logs SET end_time = NULL WHERE id = {$_POST['user']} AND date = '$today'");
         $st2 = $pdo->query("UPDATE member SET situation = 'zaishitsu' WHERE id = {$_POST['user']}");//現状の更新
         //echo "stay",$stay_time;
+        
+        //ツイート
+        $res = $connection->post("statuses/update", array("status" => $name.'さんの下校が取り消されました'));
     }
 
 
 }
+
+
+
+
+
+
+
+//レスポンス確認
+//var_dump($res);
--- time_counter.php
+++ time_counter.php
@@ -1,6 +1,20 @@
 <?php
 session_start();
 
+// OAuthライブラリの読み込み
+require "twitteroauth/autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+//認証情報４つ
+$consumerKey = "n1lm8fBMMbW5BHuxGF8MbKv5i";
+$consumerSecret = "zQtnkslApCg2PsTc9uEvvNI7KsRa79LIJtZZpRuIdcQfc7ngPq";
+$accessToken = "3221121236-DSpEQvuGhXtkv8JnCQ3COJAZLkxLcWBqmxytTU4";
+$accessTokenSecret = "J9VvLloX8yW7Re32uwPFeAzyr5Ip4OR2aZ40uSAMrkxEy";
+
+//接続
+$connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+
+
 $pdo = new PDO("mysql:dbname={$_SESSION['dbname']}", "{$_SESSION['dbusername']}", "{$_SESSION['dbpass']}");
 $today = date("Y-m-d");
 
@@ -11,7 +25,10 @@ if(!empty($_POST['come']) or (!empty($_POST['out']))){
         $st = $pdo->prepare("INSERT INTO user_logs (id, date, start_time) VALUES(?, ?, ?)");
         $st->execute(array($_SESSION['userId'], $today, $nowtime));
         
-        $st = $pdo->query("UPDATE member SET situation = 'zaishitsu' WHERE id = {$_SESSION['userId']}");//現状の更新      
+        
+        $st = $pdo->query("UPDATE member SET situation = 'zaishitsu' WHERE id = {$_SESSION['userId']}");//現状の更新
+        //ツイート
+        $res = $connection->post("statuses/update", array("status" => $_SESSION['userName'].'さんが登校しました'));
         
         //echo "登校時間を登録しました．",'<br /><a href="top.php">戻る</a>';
          header('Location: top.php');
@@ -21,6 +38,10 @@ if(!empty($_POST['come']) or (!empty($_POST['out']))){
         $st->execute(array($nowtime, $_SESSION['userId'], $today));
         
         $st = $pdo->query("UPDATE member SET situation = 'kitaku' WHERE id = {$_SESSION['userId']}");//現状の更新
+        
+        //ツイート
+        $res = $connection->post("statuses/update", array("status" => $_SESSION['userName'].'さんが下校しました'));
+
         
         //echo "下校時間を登録しました．",'<br /><a href="top.php">戻る</a>';
          header('Location: top.php');
@@ -34,6 +55,9 @@ if(!empty($_POST['come']) or (!empty($_POST['out']))){
 if(!empty($_POST['outreset'])){//下校取り消し
     $st = $pdo->query("UPDATE user_logs SET end_time = NULL WHERE id = {$_SESSION['userId']} AND date = '$today'");
     $st = $pdo->query("UPDATE member SET situation = 'zaishitsu' WHERE id = {$_SESSION['userId']}");//現状の更新
+    
+    //ツイート
+        $res = $connection->post("statuses/update", array("status" => $_SESSION['userName'].'さんの下校が取り消されました'));
     
     //echo "下校時間を取り消しました．",'<br /><a href="top.php">戻る</a>';
     header('Location: top.php');
